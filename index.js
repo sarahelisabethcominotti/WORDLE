e1115dc0554b85a26a47e8539e35c3b0ed5e16a5
sarahelisabethcominotti <138233386+sarahelisabethcominotti@users.noreply.github.com>
2024-05-26 18:15:43 +0100

    Moved checkArrays function to new JS file

--- index.js
+++ index.js
@@ -1,183 +1,104 @@
 import {fetchWord} from './fetchWord.js'
+import {checkArrays} from './checkWord.js'
 
 const word = await fetchWord()
-console.log(`word outside ${word}`)
-
 const wordArray = word.toUpperCase().split('')
-
 console.log(wordArray)
 
 
 //KEYBOARD - Assigning event listners to the buttons (elements)
-const keys = document.querySelectorAll('.key');
+const keys = document.querySelectorAll('.key')
+const del = document.getElementById('backspace')
+let cellIndex = 1;
+let firstRow = [];
+
+keys.forEach(key => {
+
+    
+    key.addEventListener('click', () => {
+        const cell = document.getElementById(`cell${cellIndex}`);
+        
+        if (key.classList.contains("delete")) {
+            if (cellIndex){
+                cellIndex--
+                const deleteLetter = document.getElementById(`cell${cellIndex}`)
+                if (deleteLetter) {
+                    deleteLetter.textContent = ''
+                    deleteLetter.classList.remove("active-cell-border")
+                    firstRow.pop()
+                }
+            }
+            return
+        }
 
 
+        if (cell) {
+            cell.textContent = key.textContent.toUpperCase();
+            cell.classList.add("active-cell-border")
+            firstRow.push(cell.textContent);
+            console.log(firstRow);
+            cellIndex++;
+        }
 
-            let cellIndex = 1;
-            let firstRow = [];
-            //let secondRow = []
+        // Check if the first row is complete
+        if (cellIndex === 6) {
+            console.log('First row complete:', firstRow);
+            if (checkArrays(wordArray, firstRow, 0)) {
+            } 
+            
+        }
+        if (cellIndex === 11) {
+            let secondRow = firstRow.slice(5,10)
+            console.log('Second row complete:', secondRow);
+            if (checkArrays(wordArray, secondRow, 5)) {
+            } 
+        }
+        if (cellIndex === 16) {
+            let thirdRow = firstRow.slice(10,15)
+            console.log('Third row complete:', thirdRow);
+            if (checkArrays(wordArray, thirdRow, 10)) {
+            }
+        }
+        if (cellIndex === 21) {
+            let fourthRow = firstRow.slice(15,20)
+            console.log('Fourth row complete:', fourthRow);
+            if (checkArrays(wordArray, fourthRow, 15)) {
+            } 
+        }
+        if (cellIndex === 26) {
+            let fifthRow = firstRow.slice(20,25)
+            console.log('Fifth row complete:', fifthRow);
+            if (checkArrays(wordArray, fifthRow, 20)) {
+            } 
+        }
+        if (cellIndex === 31) {
+            let sixthRow = firstRow.slice(25,30)
+            console.log('Sixth row complete:', sixthRow);
+            if (checkArrays(wordArray, sixthRow, 25)) {
+            } 
+            //console.log('Game over');
+        }
 
-            keys.forEach(key => {
-                key.addEventListener('click', () => {
-                    const cell = document.getElementById(`cell${cellIndex}`);
-                    if (cell) {
-                        cell.textContent = key.textContent.toUpperCase();
-                        cell.classList.add("active-cell-border")
-                        firstRow.push(cell.textContent);
-                        console.log(firstRow);
-                        cellIndex++;
-                    }
-                    
-                    //const row1 = document.querySelector(`.row1`).querySelectorAll(`.col > div`)
-                    
+        
+    });
 
-
-                    // Check if the first row is complete
-                    if (cellIndex === 6) {
-                        console.log('First row complete:', firstRow);
-                        if (checkArrays(wordArray, firstRow, 0)) {
-                            // for (let i = 1; i < firstRow.length + 1; i++) {
-                            //     const cell = document.getElementById(`cell${i}`)
-                            //     cell.classList.add("correct-cell")
-                            // }
-                    
-                            console.log("you win")
-                        } 
-                        
-                    }
-
-                    if (cellIndex === 11) {
-                        let secondRow = firstRow.slice(5,10)
-                        console.log('Second row complete:', secondRow);
-                        if (checkArrays(wordArray, secondRow, 5)) {
-                            // for (let i = 1; i < firstRow.length + 1; i++) {
-                            //     const cell = document.getElementById(`cell${i}`)
-                            //     cell.classList.add("correct-cell")
-                            // }
-                    
-                            console.log("you win")
-                        } 
-                    }
-                    if (cellIndex === 16) {
-                        let thirdRow = firstRow.slice(10,15)
-                        console.log('Third row complete:', thirdRow);
-                        if (checkArrays(wordArray, thirdRow, 10)) {
-                            // for (let i = 1; i < firstRow.length + 1; i++) {
-                            //     const cell = document.getElementById(`cell${i}`)
-                            //     cell.classList.add("correct-cell")
-                            // }
-                    
-                            console.log("you win")
-                        }
-                    }
-                    if (cellIndex === 21) {
-                        let fourthRow = firstRow.slice(15,20)
-                        console.log('Fourth row complete:', fourthRow);
-                        if (checkArrays(wordArray, fourthRow, 15)) {
-                            // for (let i = 1; i < firstRow.length + 1; i++) {
-                            //     const cell = document.getElementById(`cell${i}`)
-                            //     cell.classList.add("correct-cell")
-                            // }
-                    
-                            console.log("you win")
-                        } 
-                    }
-                    if (cellIndex === 26) {
-                        let fifthRow = firstRow.slice(20,25)
-                        console.log('Fifth row complete:', fifthRow);
-                        if (checkArrays(wordArray, fifthRow, 20)) {
-                            // for (let i = 1; i < firstRow.length + 1; i++) {
-                            //     const cell = document.getElementById(`cell${i}`)
-                            //     cell.classList.add("correct-cell")
-                            // }
-                    
-                            console.log("you win")
-                        } 
-                    }
-                    if (cellIndex === 31) {
-                        let sixthRow = firstRow.slice(25,30)
-                        console.log('Sixth row complete:', sixthRow);
-                        if (checkArrays(wordArray, sixthRow, 25)) {
-                            // for (let i = 1; i < firstRow.length + 1; i++) {
-                            //     const cell = document.getElementById(`cell${i}`)
-                            //     cell.classList.add("correct-cell")
-                            // }
-                    
-                            console.log("you win")
-                        } 
-                        console.log('Game over');
-                    }
-
-                    function checkArrays(array1, array2, index) {
-                        for (let i = index; i < index+5; i++) {
-                            if (array1[i-index] === array2[i-index]) {
-                                const cellContent = document.getElementById(`cell${i+1}`)
-                                console.log(array1[i-index], array2[i-index])
-                                //console.log(cell)
-                                
-                                cellContent.classList.add("correct-cell")
-                                }
-                        else {
-                            if (array1.includes(array2[i-index])) {
-                                console.log(array1.includes(array2[i-index]))
-                                const cellContent = document.getElementById(`cell${i+1}`)
-                                console.log(array1[i-index], array2[i-index])
-                                //console.log(cell)
-                                
-                                cellContent.classList.add("correct-letter")
-
-                            }
-                         
-                        }
-                    } }
-                });
-            });
-
-// keys.forEach(key => {
-//     key.addEventListener('click', () => {
-//         const cell = document.getElementById(`cell${cellIndex}`)
-//         let cellIndex = 1   
-//         let firstRow = []
-//         // let sedondRow = []
-//         // let thirdRow = []
-//         // let fourthRow = []
-//         // let fifthRow = []
-//         // let sixthRow = []
-//         // wordArray.forEach(checkLetter) 
-//         // function checkLetter() {
-
-//         // }
-//         if (cell) {
-//         cell.textContent = key.textContent
-//         firstRow.push(cell.textContent)
-//         console.log(firstRow)
-//         cellIndex++;
-//             if (cellIndex === 5) {
-                
-//             }
-//             if (cellIndex === 30) {
-//                 console.log(`gameover`)
-//             }
-
-//         }
-//  });
-
-// });
+   
+});
 
 
+    // del.addEventListener('click', () => {
+    //     const cell = document.getElementById(`cell${cellIndex}`);
+        
+    //     if (cell) {
+    //         const delLetter = key.textContent.toUpperCase()
+    //         cell.textContent = ''
+    //         cell.classList.add("active-cell-border")
+    //         firstRow.pop(delLetter);
+    //         console.log(firstRow);
+    //     }
 
-
-
-
-
-
-
-
-
-
-
-
-
+    
+    // });
 
 
 ////////////////////////////INPUT WORD IN CELLS////////////////////////
